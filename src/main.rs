4222622b46539bf9482b2b91ae9d1a68396f9913
dissident <moreiras@informatik.uni-freiburg.de>
2019-06-18 01:42:44 +0200

    add new importer for pure text datasets

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 
 mod reuters_importer;
+mod moviedb_importer;
 mod index;
 use std::fs::File;
 use std::io::prelude::*;
@@ -20,24 +21,26 @@ fn main() {
         // TODO: Parse all xml files
         // TODO: Parse and index oldid, newid, date, dateline
 	let base_path = PathBuf::from(r"./index");
-	let mut file = File::open(r"./schemata/reuters.json").unwrap();
+	
+	let schema = read_schema("./schemata/movies.json".to_string());
+
+	
+	let mut catalog = index::IndexCatalog::new(base_path).unwrap();
+	catalog.create_index("movies".to_string(), schema).unwrap();
+	let index = catalog.get_index(&"movies".to_string()).unwrap();
+		let articles = moviedb_importer::reader();
+	index.add_documents(&articles);
+
+}
+
+fn read_schema(path : String) -> tantivy::schema::Schema{
+	 let mut file = File::open(path).unwrap();
 	let mut buf_reader = BufReader::new(file);
   let mut contents = String::new();
   buf_reader.read_to_string(&mut contents).unwrap();
 	let schema : tantivy::schema::Schema =
   serde_json::from_str(&contents).expect("JSON was not well-formatted");
-		
-	
-	let mut catalog = index::IndexCatalog::new(base_path).unwrap();
-	catalog.create_index("reuters".to_string(), schema).unwrap();
-	let index = catalog.get_index(&"reuters".to_string()).unwrap();
-		let articles = reuters_importer::reader();
-	index.add_documents(&articles);
-
-	
-	
-
-
+	return schema
 }
 
 
--- src/moviedb_importer.rs
+++ src/moviedb_importer.rs
@@ -1,80 +1,36 @@
-use std::fs;
 use std::path::Path;
 use tantivy::Index;
 use tantivy::Result;
 use tantivy::schema::*;
 
-#[derive(Debug)]
-pub struct article {
-  title: String,
-  body: String,
-}
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
+use regex::Regex;
 
-fn read_xml(filepath : Path) -> Vec<article> {
-  let mut articles: Vec<article> = Vec::new(); 
-  let content = fs::read_to_string(filepath).unwrap();
-  let root: Element = content.parse().unwrap();
-  for child in root.children() {
-      if child.name() == "REUTERS" {
-	  let mut title = String::new();
-	  let mut body = String::new();
-	  for article_child in child.children() {
-		if article_child.name() == "TEXT" {
-			for text_child in article_child.children() {
-				if text_child.name() == "TITLE" {
-					title = text_child.text();
-				} else if text_child.name() == "BODY" {
-					body = text_child.text();
-				}
-			}
-		}
-	  }
-	  articles.push(ReutersArticle {
-		title: title, 
-		body: body,
-	  }); 
-      }
-  }
+
+pub fn reader() -> Vec<Vec<(String, Value)>> {
+let mut articles: Vec<Vec<(String, Value)>> = Vec::new();  
+let f = File::open("datasets/movies.txt").unwrap();
+let mut file = BufReader::new(&f);
+let re = Regex::new(r"[\t]+").unwrap();
+let mut count = 0;
+    for line in file.lines() {
+			let mut article: Vec<(String, Value)> = Vec::new();
+			let uline = &line.unwrap();
+			let entry : Vec<&str> = re.split(uline).collect();
+			let title = ("title".to_string(),Value::Str(entry[0].to_string()));
+			article.push(title);
+			let body = ("body".to_string(),Value::Str(entry[1].to_string()));
+			article.push(body);
+			let id = ("id".to_string(), Value::U64(count));
+			article.push(id);
+			count += 1;
+				
+    }   
+
+	
   return articles;
 }
 
-fn create_index() -> Result<tantivy::Index> {
-	// create schema
-	let mut schema_builder = SchemaBuilder::default();
-	schema_builder.add_text_field("title", TEXT | STORED);
-	schema_builder.add_text_field("body", TEXT | STORED);
-	let schema = schema_builder.build();
 
-	// create index	
-	let index = match Index::create_in_dir(Path::new("./index/"), schema.clone()) {
-		Ok(index) => index,
-		Err(e)    => panic!("Error: {}", e),
-	};
-	Ok(index)
-}
-
-fn index_articles(index: tantivy::Index, articles: Vec<ReutersArticle>) -> Result<()> {
-	let mut index_writer = index.writer(50_000_000)?;
-	let schema = index.schema();
-	let title = schema.get_field("title").unwrap();
-	let body = schema.get_field("body").unwrap();
-
-	for article in articles {
-		println!("Title{},title");
-		let mut doc = Document::default();
-		doc.add_text(title, &article.title);
-		doc.add_text(body, &article.body);
-		index_writer.add_document(doc);
-	}
-	
-	index_writer.commit();
-
-	Ok(())	
-}
-
-fn main() {
-	let articles = read_xml();
-	
-	let index = create_index().unwrap();
-	index_articles(index, articles);
-}
